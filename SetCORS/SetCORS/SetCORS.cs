9560c7ea70d066d77fef3d1e11cca09a6e4654f3
jobeland <jordan.beland@gmail.com>
2016-05-18 12:03:28 -0700

    add gist clipping + pull down nuget packages

---
+++ SetCORS/SetCORS/SetCORS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+
+namespace SetCORS
+{
+    class SetCors
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = File.ReadAllText("C:\\ConnectionStrings\\StorageAccountConnectionString.txt");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            var corsRule = new CorsRule()
+            {
+                AllowedHeaders = new List<string>() { "*" },
+                AllowedMethods = CorsHttpMethods.Put | CorsHttpMethods.Get | CorsHttpMethods.Head | CorsHttpMethods.Post,
+                AllowedOrigins = new List<string>() { "*" },
+                ExposedHeaders = new List<string>() { "*" },
+                MaxAgeInSeconds = int.MaxValue
+            };
+            var serviceProperties = blobClient.GetServiceProperties();
+            var corsSettings = serviceProperties.Cors;
+            corsSettings.CorsRules.Add(corsRule);
+            blobClient.SetServiceProperties(serviceProperties);
+        }
+    }
+}
